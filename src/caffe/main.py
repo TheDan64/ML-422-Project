8f8f944daed56dd7c0246c061498039c2023462a
TheDan64 <ThaDan64@gmail.com>
2015-04-13 18:43:38 -0400

    Latest working code.

--- src/caffe/main.py
+++ src/caffe/main.py
@@ -5,5 +5,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import os
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--buildmodel")
+
+args = parser.parse_args()
+
+# Globals and Paths
+MODEL_DEFINITION = ""
+PRETRAINED_MODEL = "models/model.caffemodel"
+IMG_TRAIN_PATH = "../../data/sample" # "../../data/train"
+IMG_TEST_PATH = "../../data/test"
+LABELS = "../../data/trainLabels.csv"
+
+caffe.set_mode_cpu()
+
+if args.buildmodel:
+	if os.path.isfile(args.buildmodel):
+		print("Building model to " + PRETRAINED_MODEL)
+		solver = caffe.SGDSolver(args.buildmodel)
+
+	else:
+		print(args.buildmodel + " is not a valid file!")
+
+	exit()
+
+
+
+if not os.path.isfile(PRETRAINED_MODEL):
+	print("No pretrained model found!")
+
+	exit()
+
+#solver = caffe.SGDSolver("lenet_solver.prototxt")
 print("Hello, world!")
